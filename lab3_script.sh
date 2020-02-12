a978e5addc641723b94629d2527e4a50cf71ff8d
ishakarki <iska6979@colorado.edu>
2020-02-11 17:22:28 -0700

    final submission

--- lab3_script.sh
+++ lab3_script.sh
@@ -13,13 +13,16 @@ read regEx
 grep $regEx $fileName
 
 # count number of phone numbers
-grep ^...-...-....$ regex_practice.txt -c
+echo "A count of the number of phone numbers: "
+egrep [0-9]{3}-[0-9]{3}-[0-9]{4} regex_practice.txt -c
 
 # count number of emails (anything with an @)
-grep @ regex_practice.txt -c
+echo "A count of the number of emails: "
+grep '^[a-zA-Z0-9._]*[@][a-zA-Z0-9._]*[.][a-zA-Z0-9]*' regex_practice.txt -c
 
 # 303 phone numbers
-grep ^303-...-....$ regex_practice.txt -o
+echo "A list of all the phone numbers in the 303 area code: "
+egrep 303-[0-9]{3}-[0-9]{4} regex_practice.txt -o
 
 # @geocities
 grep @geocities.com regex_practice.txt >> email_results.txt
